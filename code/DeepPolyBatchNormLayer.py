13a6fd78a995720ab99c80f6f5d2c302fa54928c
Angelo Gnazzo <gnazzoangelo@gmail.com>
2022-12-15 09:51:42 +0100

    non funzia

--- code/DeepPolyBatchNormLayer.py
+++ code/DeepPolyBatchNormLayer.py
@@ -23,6 +23,17 @@ class DeepPolyBatchNormLayer(torch.nn.Module):
         
     def forward(self, x, lower_bound, upper_bound, gamma, beta, mean, var, eps, input_shape):
         
+        # compute the weights and bias
+        var_sqrt = torch.sqrt(var + eps)
+
+        w_prime = gamma / var_sqrt
+        b = (- mean * gamma) / var_sqrt + beta
+        mult=torch.ones(1, input_shape[0]*input_shape[2]*input_shape[3])
+        w= torch.matmul(w_prime.reshape(-1, 1), mult).flatten()
+        self.weights = torch.diag(w)
+        self.bias = b.repeat(input_shape[0]*input_shape[2]*input_shape[3])
+        
+        
         x = x.reshape(input_shape)
         lower_bound = lower_bound.reshape(input_shape)
         upper_bound = upper_bound.reshape(input_shape)
@@ -31,17 +42,13 @@ class DeepPolyBatchNormLayer(torch.nn.Module):
             print("DeepPolyBatchNormLayer: x shape %s, lower_bound shape %s, upper_bound shape %s" % (
                 str(x.shape), str(lower_bound.shape), str(upper_bound.shape)))
         
-        var_sqrt = torch.sqrt(var + eps)
 
-        w = torch.div(gamma,var_sqrt)
-        b = (- mean * gamma) / var_sqrt + beta
-
-        self.weights = torch.diag(w)
-        self.bias = b.reshape(1, -1)
         # perform batch normalization on the actual input
         x= self.layer(x)
         lower_bound = self.layer(lower_bound)
         upper_bound = self.layer(upper_bound)
+        
+
 
         return x, lower_bound, upper_bound, input_shape
     
